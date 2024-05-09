5be9528005b9a75df45a07164612c4323cfdd1ce
Tzvi Gregory Kaidanov <tzvi.kaidanov@made4net.com>
2024-05-09 19:27:51 +0300

    Add .gitignore

---
+++ public/chat.js
@@ -0,0 +1,34 @@
+var socket = io();
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+var messages = document.getElementById('messages');
+var username = document.getElementById('username');
+var loginDiv = document.getElementById('login');
+var chatDiv = document.getElementById('chat');
+var topics = document.getElementById('topics');
+
+function login() {
+    console.log('Logging in with username:', username.value);
+    socket.emit('login', username.value);
+    loginDiv.style.display = 'none';
+    chatDiv.style.display = 'block';
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (input.value) {
+        console.log('Sending message:', input.value);
+        socket.emit('chat message', { topic: topics.value, message: input.value });
+        input.value = '';
+    }
+});
+
+socket.on('chat message', function(msg) {
+    if (msg.topic === topics.value) {
+        console.log('Received message:', msg);
+        var item = document.createElement('li');
+        item.textContent = msg.user + ': ' + msg.message;
+        messages.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+});
---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let users = {};
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Serve 'index.html' when a GET request is made to the root URL ("/")
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected: ' + socket.id);
+
+    socket.on('login', (username) => {
+        users[socket.id] = username;
+    });
+
+    socket.on('chat message', (msg) => {
+        const username = users[socket.id];
+        if (!username) {
+            return socket.emit('error', { message: 'No username set' });
+        }
+        io.emit('chat message', { user: username, topic: msg.topic, message: msg.message });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected: ' + socket.id);
+        delete users[socket.id];
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
